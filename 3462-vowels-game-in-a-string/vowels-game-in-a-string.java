f9f1fe1963f7d3ef8811317f3adde7dc4959d6d0
Bose B <boseb1274@gmail.com>
2025-09-19 22:32:00 +0530

    Time: 11 ms (41.24%) | Memory: 45.4 MB (80.27%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean doesAliceWin(String s) {
         String vowel="aeiou";
-        int count=0;
         for(char c:s.toCharArray()){
             if(vowel.indexOf(c)!=-1)return true;
         }
