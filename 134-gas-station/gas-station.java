0fe4a008fc916617dce0fb403d7cc58d464bb8de
Bose B <boseb1274@gmail.com>
2025-09-21 01:09:16 +0530

    Time: 2 ms (97.14%) | Memory: 56.5 MB (31.02%) - LeetSync

--- 134-gas-station/gas-station.java
+++ 134-gas-station/gas-station.java
@@ -1,12 +1,11 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int gas_sum=0,cost_sum=0;
+        int gas_sum=0,cost_sum=0,total=0,res=0;
         for(int i=0;i<gas.length;i++){
             gas_sum+=gas[i];
             cost_sum+=cost[i];
         }
         if(gas_sum < cost_sum)return -1;
-        int total=0,res=0;
         for(int i=0;i<gas.length;i++){
             total+=gas[i]-cost[i];
             if(total<0){
@@ -15,9 +14,6 @@ class Solution {
             }
         }
         return res;
-
-
-
     }
     /*private int sum(int[] arr){
         int total=0;
